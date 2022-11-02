f9a55ba39f4b857553030b441f7a484f1ab9c5dc
Abhishek k <70311489+Abhishekkannath@users.noreply.github.com>
2022-11-02 22:56:58 +0530

    Add files via upload

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "cse";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$servername="localhost";
+$username="root";
+$password="";
+
+$conn=mysql_connect($servername,$username,$password);
 if(!$conn)
   echo "Error in Connection".mysql_error();
 
-$dbcheck=mysql_select_db("cse");
+$dbcheck=mysql_select_db("lname");
 if(!$dbcheck)
 	echo "Error selecting Database<br>".mysql_error();
 //else echo "Success";
