8c01fa9b68626cd16feeedb4592bde5ea5716ee6
Abhishek k <70311489+Abhishekkannath@users.noreply.github.com>
2022-11-05 11:34:38 +0530

    database connection

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
+$servername="sql12.freesqldatabase.com";
+$username="sql12530642";
+$password="f4nTtWu2BS";
 
 $conn=mysql_connect($servername,$username,$password);
 if(!$conn)
   echo "Error in Connection".mysql_error();
 
-$dbcheck=mysql_select_db("lname");
+$dbcheck=mysql_select_db("sql12530642");
 if(!$dbcheck)
 	echo "Error selecting Database<br>".mysql_error();
 //else echo "Success";
